c06370f7c746da7ee8c45041735973fb9cba5bc6
miguelbalmeida <miguelbalmeida@gmail.com>
2012-08-22 18:11:40 +0100

    Added support for computing ROUGE scores of several guessed summaries at once; this involved a slight change in the input format, see Example.py for an example of usage.

--- Example.py
+++ Example.py
@@ -1,5 +1,7 @@
 import PythonROUGE
 
-guess_summary_file = 'Example/Guess_Summ_1.txt'
-ref_summary_file_list = ['Example/Ref_Summ_1.txt','Example/Ref_Summ_2.txt']
-recall,precision,F_measure = PythonROUGE.PythonROUGE(guess_summary_file,ref_summary_file_list)
+guess_summary_list = ['Example/Guess_Summ_1.txt','Example/Guess_Summ_2.txt']
+ref_summary_list = [['Example/Ref_Summ_1_1.txt','Example/Ref_Summ_1_2.txt'] , ['Example/Ref_Summ_2_1.txt','Example/Ref_Summ_2_2.txt','Example/Ref_Summ_2_3.txt']]
+recall,precision,F_measure = PythonROUGE.PythonROUGE(guess_summary_list,ref_summary_list,ngram_order=2)
+
+print recall,precision,F_measure
--- PythonROUGE.py
+++ PythonROUGE.py
@@ -10,24 +10,32 @@ import re
 
 # Wrapper function to use ROUGE from Python easily
 # Inputs:
-    # guess_summ_file, a string with the absolute path to the file with your guess summary
-    # ref_summ_list, a list of paths to multiple reference summaries.
+    # guess_summ_list, a string with the absolute path to the file with your guess summary
+    # ref_summ_list, a list of lists of paths to multiple reference summaries.
     # IMPORTANT: all the reference summaries must be in the same directory!
     # (optional) ngram_order, the order of the N-grams used to compute ROUGE
     # the default is 1 (unigrams)
 # Output: a tuple of the form (recall,precision,F_measure)
 #
 # Example usage: PythonROUGE('/home/foo/my_guess_summary.txt',[/home/bar/my_ref_summary_1.txt,/home/bar/my_ref_summary_2.txt])
-def PythonROUGE(guess_summ_file,ref_summ_list,ngram_order=2):
+def PythonROUGE(guess_summ_list,ref_summ_list,ngram_order=2):
     """ Wrapper function to use ROUGE from Python easily. """
-    
-    # even though we ask that the second argument is a list,
+
+    # even though we ask that the first argument is a list,
     # if it is a single string we can handle it
-    if type(ref_summ_list) == str:
+    if type(guess_summ_list) == str:
         temp = list()
         temp.append(ref_summ_list)
-        ref_summ_list = temp
+        guess_summ_list = temp
         del temp
+    
+    # even though we ask that the second argument is a list of lists,
+    # if it is a single string we can handle it
+#    if type(ref_summ_list[0]) == str:
+#        temp = list()
+#        temp.append(ref_summ_list)
+#        ref_summ_list = temp
+#        del temp
     
     # this is the path to your ROUGE distribution
     ROUGE_path = '/home/miguel/PythonROUGE/RELEASE-1.5.5/ROUGE-1.5.5.pl'
@@ -40,7 +48,15 @@ def PythonROUGE(guess_summ_file,ref_summ_list,ngram_order=2):
     # this is a temporary XML file which will contain information
     # in the format ROUGE uses
     xml_path = 'temp.xml'
-    create_xml(xml_path,guess_summ_file,ref_summ_list)
+    xml_file = open(xml_path,'w')
+    xml_file.write('<ROUGE-EVAL version="1.0">\n')
+    for guess_summ_index,guess_summ_file in enumerate(guess_summ_list):
+        xml_file.write('<EVAL ID="' + str(guess_summ_index+1) + '">\n')
+        create_xml(xml_file,guess_summ_file,ref_summ_list[guess_summ_index])
+        xml_file.write('</EVAL>\n')
+    xml_file.write('</ROUGE-EVAL>\n')
+    xml_file.close()
+    
     
     # this is the file where the output of ROUGE will be stored
     ROUGE_output_path = 'ROUGE_result.txt'
@@ -71,7 +87,7 @@ def PythonROUGE(guess_summ_file,ref_summ_list,ngram_order=2):
     
     # remove temporary files which were created
     os.remove(xml_path)
-#    os.remove(ROUGE_output_path)
+    os.remove(ROUGE_output_path)
 
     return (recall_list,precision_list,F_measure_list)
     
@@ -79,10 +95,7 @@ def PythonROUGE(guess_summ_file,ref_summ_list,ngram_order=2):
 # This is an auxiliary function
 # It creates an XML file which ROUGE can read
 # Don't ask me how ROUGE works, because I don't know!
-def create_xml(xml_path,guess_summ_file,ref_summ_list):
-    xml_file = open(xml_path,'w')
-    xml_file.write('<ROUGE-EVAL version="1.0">\n')
-    xml_file.write('<EVAL ID="1">\n')
+def create_xml(xml_file,guess_summ_file,ref_summ_list):
     xml_file.write('<PEER-ROOT>\n')
     guess_summ_dir = os.path.dirname(guess_summ_file)
     xml_file.write(guess_summ_dir + '\n')
@@ -104,18 +117,13 @@ def create_xml(xml_path,guess_summ_file,ref_summ_list):
         xml_file.write('<M ID="' + letter_list[ref_summ_index] + '">' + ref_summ_basename + '</M>\n')
     
     xml_file.write('</MODELS>\n')
-    xml_file.write('</EVAL>\n')
-    xml_file.write('</ROUGE-EVAL>\n')
-    xml_file.close()
     
 # This is only called if this file is executed as a script.
 # It shows an example of usage.
 if __name__ == '__main__':
-    guess_summ_file = 'Example/Guess_Summ_1.txt'
-    ref_summ_list = list()
-    ref_summ_list.append('Example/Ref_Summ_1.txt')
-    ref_summ_list.append('Example/Ref_Summ_2.txt')
-    recall_list,precision_list,F_measure_list = PythonROUGE(guess_summ_file,ref_summ_list)
+    guess_summary_list = ['Example/Guess_Summ_1.txt','Example/Guess_Summ_2.txt']
+    ref_summ_list = [['Example/Ref_Summ_1_1.txt','Example/Ref_Summ_1_2.txt'] , ['Example/Ref_Summ_2_1.txt','Example/Ref_Summ_2_2.txt','Example/Ref_Summ_2_3.txt']]
+    recall_list,precision_list,F_measure_list = PythonROUGE(guess_summary_list,ref_summ_list)
     print 'recall = ' + str(recall_list)
     print 'precision = ' + str(precision_list)
     print 'F = ' + str(F_measure_list)
